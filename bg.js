933f6b534431cbc380691176c697026dc6325553
dkmcgowan <dmcgowan@kabbage.com>
2014-11-13 10:27:35 -0500

    added support for x-requested-domain

--- bg.js
+++ bg.js
@@ -24,19 +24,26 @@ var modifyHeader = function (tabId, url, allHeaders) {
 		var hostname = getHostname(url);
 		var ret = allHeaders.slice();
 		var newhostname = hostname.replace(/-qa[0-9]{1}/, "-qa");
+		
+		var requestedDomain = newhostname;
 				
 		if(oData[PARTNER_KEY] != undefined) {
 			var urlmatch = newhostname.match(/assets-qa.*\.kabbage.com/);
 			if (urlmatch == null) {
 				var dataId = getIdFromArray(oData[PARTNER_KEY], tabId);
 				if (dataId != -1) {
-					newhostname = newhostname.replace(/kabbage.com/, oData[PARTNER_KEY][dataId].partnerName);
+					requestedDomain = newhostname.replace(/kabbage.com/, oData[PARTNER_KEY][dataId].partnerName);
 				}
 			}
 		}
 		
+		//this will come out once we fully utilize requested domain
+		newhostname = requestedDomain;
+		
 		console.log('host=' + newhostname + ' for ' + url);
+		
 		ret.push({name:"Host", value:newhostname});
+		ret.push({name:"X-Requested-Domain", value:requestedDomain});
 	}
     return ret;
 }
